d16b0dfc91566fe44305adc5c02842bfc917c75f
Pedro <ppineiroordax@danielcastelao.org>
2025-01-25 21:51:32 +0100

    arreglo datos

--- app/src/main/java/com/pedro/simondicemvvm/datos/Datos.kt
+++ app/src/main/java/com/pedro/simondicemvvm/datos/Datos.kt
@@ -1,10 +1,10 @@
 package com.pedro.simondicemvvm.datos
 
 object Datos {
-    var aciertos = 0 // Número de aciertos del usuario.
-    var rondas = 0 // Número de rondas completadas.
-    var numRandom = 0 // Número aleatorio generado por la máquina.
-    var record = 0 // Récord máximo del jugador.
-    val listaNumerosRandom : MutableList<Int> = mutableListOf() // Secuencia generada.
-    var listaColores : MutableList<Int> = mutableListOf() // Respuestas del jugador.
+    var aciertos = 0 // Número de aciertos del usuario
+    var rondas = 0 // Número de rondas completadas
+    var numRandom = 0 // Número aleatorio generado por la máquina
+    var record = 0 // Récord máximo del jugador
+    val listaNumerosRandom : MutableList<Int> = mutableListOf() // Secuencia generada
+    var listaColores : MutableList<Int> = mutableListOf() // Respuestas del jugador
 }

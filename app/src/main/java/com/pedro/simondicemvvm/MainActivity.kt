412a10a1358445e600488ac9318841d01fd2b5a4
Pedro <ppineiroordax@danielcastelao.org>
2025-01-25 17:10:54 +0100

    MainActivity actualizado

--- app/src/main/java/com/pedro/simondicemvvm/MainActivity.kt
+++ app/src/main/java/com/pedro/simondicemvvm/MainActivity.kt
@@ -19,29 +19,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimonDiceMVVMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SimonDiceUI()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SimonDiceMVVMTheme {
-        Greeting("Android")
+        SimonDiceUI()
     }
 }
